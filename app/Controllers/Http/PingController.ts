a64bf331df5f4315bd5c25092583b3d7dd5acac6
Unknown User <jumperkylian@outlook.com>
2023-12-09 12:30:46 +0100

    feat(api): all-base files

---
+++ app/Controllers/Http/PingController.ts
@@ -0,0 +1,12 @@
+// app/Controllers/Http/PingController.ts
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { DateTime } from 'luxon';
+
+export default class PingController {
+  public async handle({ response }: HttpContextContract) {
+    const start = DateTime.now();
+    const end = DateTime.now();
+    const duration = end.diff(start, 'milliseconds');
+    return response.send({ status: 'pong', duration: duration.milliseconds });
+  }
+}
---
+++ start/routes.ts
@@ -0,0 +1,37 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async () => {return { TogetherAPI: 'Good !' }})
+
+Route.group(() => {
+  Route.get('/ping', 'PingController.handle')
+}).prefix('/manager')
+
+Route.group(() => {
+  Route.post('/containers/create', 'ContainersController.create');
+  Route.post('/containers/delete', 'ContainersController.delete');
+  Route.post('/containers/stop', 'ContainersController.stop');
+  Route.post('/containers/start', 'ContainersController.start');
+  Route.post('/containers/restart', 'ContainersController.restart');
+  Route.post('/containers/status', 'ContainersController.status');
+}).prefix('/instance')
+
