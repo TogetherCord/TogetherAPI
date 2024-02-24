397c37d7baf468d85fd67f48b7bec074162217f5
Unknown User <jumperkylian@outlook.com>
2024-02-24 17:24:54 +0100

    fix(api): Fix Upload/Download Routes

--- app/Controllers/Http/ReceiveController.ts
+++ app/Controllers/Http/ReceiveController.ts
@@ -8,48 +8,48 @@ const unlink = promisify(fs.unlink);
 const access = promisify(fs.access);
 const readFile = promisify(fs.readFile);
 import path from 'path'
-import { Response } from '@adonisjs/http-server/build/standalone'
 
 export default class ReceiveController {
-    public async upload({ request }: HttpContextContract) {
-        const file = request.file('file');
+  public async upload({ request }: HttpContextContract) {
+    const file = request.file('file');
 
-        if (!file) {
-            return { status: 'error', message: 'No file uploaded' };
-        }
-
-        const tmpPath = Application.tmpPath('uploads');
-        await file.move(tmpPath);
-
-        const fileContent = await readFile(path.join(tmpPath, file.fileName), 'utf8');
-
-        await unlink(path.join(tmpPath, file.fileName));
-
-        const filePath = Application.tmpPath('friends.json');
-        await writeFile(filePath, fileContent, 'utf8');
-
-        return { status: 'success' };
+    if (!file) {
+      return { status: 'error', message: 'No file uploaded' };
     }
 
-    public async download({ response }: HttpContextContract) {
-        const filePath = Application.tmpPath('friends.json');
+    const tmpPath = Application.tmpPath('uploads');
+    await file.move(tmpPath);
 
-        try {
-            await access(filePath);
-        } catch {
-            return response.status(404).send({ status: 'error', message: 'File not found' });
-        }
+    const safeFileName = path.basename(file.fileName || 'default.txt');
+    const fileContent = await readFile(path.join(tmpPath, safeFileName), 'utf8');
 
-        const fileStream = fs.createReadStream(filePath);
+    await unlink(path.join(tmpPath, safeFileName));
 
-        response.stream(fileStream);
+    const filePath = Application.tmpPath('friends.json');
+    await writeFile(filePath, fileContent, 'utf8');
 
-        fileStream.on('end', async () => {
-            try {
-                await unlink(filePath);
-            } catch (err) {
-                console.error(`Failed to delete file: ${err}`);
-            }
-        });
+    return { status: 'success' };
+  }
+
+  public async download({ response }: HttpContextContract) {
+    const filePath = Application.tmpPath('friends.json');
+
+    try {
+      await access(filePath);
+    } catch {
+      return response.status(404).send({ status: 'error', message: 'File not found' });
     }
+
+    const fileStream = fs.createReadStream(filePath);
+
+    response.stream(fileStream);
+
+    fileStream.on('end', async () => {
+      try {
+        await unlink(filePath);
+      } catch (err) {
+        console.error(`Failed to delete file: ${err}`);
+      }
+    });
+  }
 }
